a47ed050cc651f112b026ba0e9712216042c3698
Aindrila Bhattacharjee <aindrilabhattacharjee72@gmail.com>
2024-10-10 19:50:33 +0530

    url update

--- app/configure/preview/actions.ts
+++ app/configure/preview/actions.ts
@@ -81,10 +81,8 @@ console.log(configuration)
   })
 
   const stripeSession = await stripe.checkout.sessions.create({
-    //success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
-    //cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
-    success_url: `http://localhost:3000/thank-you?orderId=${order.id}`,
-    cancel_url: `http://localhost:3000/configure/preview?id=${configuration.id}`,
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId=${order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
     //payment_method_types: ['card', 'paypal'],
     payment_method_types: ['card'],
     mode: 'payment',
